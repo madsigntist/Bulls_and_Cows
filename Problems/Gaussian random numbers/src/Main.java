bcfa466ea786fad18b51da58ba2ed3546ba920c1
Michael Kelly <64117636+madsigntist@users.noreply.github.com>
2021-09-28 20:47:16 -0700

    Delete Problems directory

--- Problems/Gaussian random numbers/src/Main.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        // write your code here
-    }
-}
--- Problems/Getting input/src/Main.java
+++
@@ -1,7 +0,0 @@
-
-class InputClass {
-    public static String getString() {
-        // write your code here
-        return new java.util.Scanner(System.in).nextLine();
-    }
-}
